5e54a5a096ea75e8297a1c3d5d6dc6fffbbaf14b
Peter Hua <pehua01@gmail.com>
2015-09-28 22:15:49 -0500

    Update for `jdee-emacs/jdee'.

--- prj.el
+++ prj.el
@@ -1,112 +1,129 @@
-(jde-project-file-version "1.0")
-(jde-set-variables
- '(jde-jdk-registry (quote (("1.6.0" . "$JAVA_HOME"))))
- '(jde-jdk (quote ("1.6.0")))
- '(jde-jdk-doc-url "http://download.oracle.com/javase/6/docs/api")
- '(jde-enable-abbrev-mode t)
- '(jde-gen-k&r t)
- '(jde-gen-cflow-enable t)
- '(jde-complete-function (quote jde-complete-minibuf))
- '(jde-electric-return-p t)
- '(jde-import-auto-collapse-imports nil)
- '(jde-import-auto-sort t)
- '(jde-import-sorted-groups (quote gor))
+(jdee-project-file-version "1.0")
+(jdee-set-variables
+ '(jdee-jdk-registry (quote (("1.7" . "/usr/lib/jvm/java-7-openjdk")
+                            ("1.8" . "/usr/lib/jvm/java-8-openjdk"))))
+ '(jdee-jdk (quote ("1.7")))
+ '(jdee-jdk-doc-url "http://docs.oracle.com/javase/7/docs/api")
+ '(jdee-help-docsets (quote ((nil "http://docs.oracle.com/javase/7/docs/api" "1.7")
+                            (nil "http://docs.oracle.com/javase/8/docs/api" "1.8")
+                            (nil "https://developer.android.com/reference"))))
+ '(jdee-enable-abbrev-mode t)
+ '(jdee-gen-k&r t)
+ '(jdee-gen-cflow-enable t)
+ '(jdee-complete-function (quote jdee-complete-minibuf))
+ '(jdee-electric-return-p t)
+ '(jdee-import-auto-collapse-imports nil)
+ '(jdee-import-auto-sort t)
+ '(jdee-import-sorted-groups (quote gor))
 
- '(jde-project-context-switching-enabled-p t)
+ '(jdee-project-context-switching-enabled-p t)
+
+ ;; '(jdee-expand-classpath-p t)
+ ;; '(jdee-lib-directory-names (quote ("/lib$" "/jar$")))
+
+ '(jdee-server-dir "$HOME/workspaces/forks/jdee-server/target")
 
  ;; JDE Compile Option Group
 
- '(jde-compiler (quote ("javac-server" "")))
- '(jde-compile-option-command-line-args nil)
- '(jde-read-compile-args nil)
- '(jde-compile-option-directory "./target/classes")
- '(jde-compile-option-deprecation t)
- '(jde-compile-option-debug (quote ("all" (t t t))))
- '(jde-compile-option-optimize nil)
- '(jde-compile-option-depend nil)
- '(jde-compile-option-vm-args nil)
- '(jde-compile-option-verbose t)
- '(jde-compile-option-nowarn nil)
+ '(jdee-compiler (quote ("javac-server" "")))
+ '(jdee-compile-option-command-line-args nil)
+ '(jdee-read-compile-args nil)
+ '(jdee-compile-option-directory "./target/classes")
+ '(jdee-compile-option-deprecation t)
+ '(jdee-compile-option-debug (quote ("all" (t t t))))
+ '(jdee-compile-option-optimize nil)
+ '(jdee-compile-option-depend nil)
+ '(jdee-compile-option-vm-args nil)
+ '(jdee-compile-option-verbose t)
+ '(jdee-compile-option-nowarn nil)
+ '(jdee-compile-option-bootclasspath nil)
 
- '(jde-compile-enable-kill-buffer nil)
+ '(jdee-compile-enable-kill-buffer nil)
 
  ;; JDE Build Option Group
 
- '(jde-build-function (quote (jde-ant-build)))
+ '(jdee-build-function (quote jdee-ant-build))
 
- '(jde-ant-invocation-method (quote ("Script")))
- '(jde-ant-home "$ANT_HOME")
- '(jde-ant-user-jar-files "$ANT_CONTRIB")
- '(jde-ant-program "ant")
- '(jde-ant-args "-emacs")
- '(jde-ant-buildfile "build.xml")
- '(jde-ant-read-buildfile nil)
- '(jde-ant-read-target t)
- '(jde-ant-interactive-buildfile "build.xml")
- '(jde-ant-read-args nil)
- '(jde-ant-enable-find t)
- '(jde-ant-complete-target t)
+ '(jdee-ant-invocation-method (quote ("Script")))
+ '(jdee-ant-home "$ANT_HOME")
+ '(jdee-ant-user-jar-files "$ANT_CONTRIB_HOME/ant-contrib-1.0b3.jar")
+ '(jdee-ant-program "ant")
+ '(jdee-ant-args "-emacs")
+ '(jdee-ant-buildfile "build.xml")
+ '(jdee-ant-read-buildfile nil)
+ '(jdee-ant-read-target t)
+ '(jdee-ant-interactive-buildfile "build.xml")
+ '(jdee-ant-read-args nil)
+ '(jdee-ant-enable-find t)
+ '(jdee-ant-complete-target t)
 
  ;; JDE Run Option Group
 
- '(jde-run-working-directory ".")
- '(jde-run-application-class "com.example.Main")
- '(jde-run-read-vm-args nil)
- '(jde-run-read-app-args nil)
- '(jde-run-option-verbose (quote (t t t)))
- '(jde-run-option-properties nil)
- ;; '(jde-run-option-heap-size (quote ((1 . "megabytes") (16 . "megabytes"))))
- ;; '(jde-run-option-stack-size (quote ((128 . "kilobytes") (400 . "kilobytes"))))
- ;; '(jde-run-option-garbage-collection (quote (t t)))
- ;; '(jde-run-option-java-profile (quote (t . "./java.prof")))
- ;; '(jde-run-option-heap-profile (quote (t "./java.hprof" 4 20 "Allocation objects")))
- '(jde-run-option-verify (quote (nil t)))
- '(jde-run-option-vm-args (quote ("-Xdebug" "-Xrunjdwp:transport=dt_socket,address=6001,server=y,suspend=n"
-				  "-Xnoagent" "-Djava.compiler=NONE")))
- '(jde-run-option-application-args nil)
+ '(jdee-run-working-directory ".")
+ '(jdee-run-application-class "com.example.Main")
+ '(jdee-run-read-vm-args nil)
+ '(jdee-run-read-app-args nil)
+ '(jdee-run-option-verbose (quote (t t t)))
+ '(jdee-run-option-properties nil)
+ ;; '(jdee-run-option-heap-size (quote ((1 . "megabytes") (16 . "megabytes"))))
+ ;; '(jdee-run-option-stack-size (quote ((128 . "kilobytes") (400 . "kilobytes"))))
+ ;; '(jdee-run-option-garbage-collection (quote (t t)))
+ ;; '(jdee-run-option-java-profile (quote (t . "./java.prof")))
+ ;; '(jdee-run-option-heap-profile (quote (t "./java.hprof" 5 20 "Allocation objects")))
+ '(jdee-run-option-verify (quote (nil t)))
+ ;; '(jdee-run-option-vm-args (quote ("-Xdebug" "-Xrunjdwp:transport=dt_socket,address=6001,server=y,suspend=n"
+ ;;               "-Xnoagent" "-Djava.compiler=NONE")))
+ ;; '(jdee-run-option-debug (quote ("Server" "Socket" "javadebug" "localhost" "6001" "Yes")))
+ '(jdee-run-option-application-args nil)
 
- ;; '(jde-run-option-debug (quote ("Server" "Socket" "javadebug" "localhost" "6001" "Yes")))
- 
  ;; JDE Debug Option Group
 
- '(jde-debugger (quote ("jdb")))
- '(jde-db-read-vm-args nil)
- '(jde-db-read-app-args nil)
- '(jde-db-option-application-args nil)
- '(jde-db-option-vm-args nil)
- '(jde-db-option-host nil)
- 
+ '(jdee-debugger (quote ("jdb")))
+ '(jdee-db-read-vm-args nil)
+ '(jdee-db-read-app-args nil)
+ '(jdee-db-option-vm-args nil)
+ '(jdee-db-option-application-args nil)
+ '(jdee-db-option-host nil)
+
+ ;; (jdee-db-option-connect-socket '(nil "28380"))
+
+ ;; JDE JUnit Option Group
+
+ '(jdee-junit-working-directory jdee-run-working-directory)
+ ;; '(jdee-junit-testrunner-type "junit.textui.TestRunner")
+ '(jdee-junit-testrunner-type "org.junit.runner.JUnitCore")
+ '(jdee-junit-tester-name-tag '("Test" . "Suffix"))
+
  ;; JDE Autocode Option Group
 
- '(jde-gen-buffer-boilerplate
+ '(jdee-gen-buffer-boilerplate
    (quote ("/**"
- 	   " * Describe class here."
-	   " *"
-	   " * @author phua"
-	   " * @verison 1.0"
-	   " */")))
+           " * Describe class here."
+           " *"
+           " * @author phua"
+           " * @version 1.0"
+           " */")))
  )
 
 ;; JDE Cross-Reference Database
 
-;; '(jde-sourcepath (quote ("./src/main/java" "./src/test/java")))
-;; '(jde-global-classpath (quote ("./src/main/resources" "./target/classes" "/usr/share/java")))
-;; '(jde-built-class-path (quote ("./target/classes" "./target/test/classes")))
-;; '(jde-xref-db-base-directory "./xrefdb")
-;; '(jde-xref-store-prefixes (quote ("com.example")))
-;; '(jde-xref-cache-size 3))
+;; '(jdee-xref-db-base-directory "./xrefdb")
+;; '(jdee-xref-store-prefixes (quote ("com.example")))
+;; '(jdee-xref-cache-size 3))
 
-(jde-gen-define-abbrev-template "melse"  '('> "else {" '> 'n '> 'r 'n "}"))
+;; JDE Abbreviation Templates
 
-(jde-gen-define-abbrev-template "sysin"  '("System.in.readline();"))
-(jde-gen-define-abbrev-template "sysout" '("System.out.println(\"" (p "x:") "\");"))
-(jde-gen-define-abbrev-template "syserr" '("System.err.println(\"" (p "x:") "\");"))
-(jde-gen-define-abbrev-template "trace"  '("logger.trace(\"" (p "Message:") "\");"))
-(jde-gen-define-abbrev-template "debug"  '("logger.debug(\"" (p "Message:") "\");"))
-(jde-gen-define-abbrev-template "info"   '("logger.info(\"" (p "Message:") "\");"))
-(jde-gen-define-abbrev-template "warn"   '("logger.warn(\"" (p "Message:") "\");"))
-(jde-gen-define-abbrev-template "error"  '("logger.error(\"" (p "Message:") "\");"))
-(jde-gen-define-abbrev-template "fatal"  '("logger.fatal(\"" (p "Message:") "\");"))
+(jdee-gen-define-abbrev-template "melse"  '('> "else {" '> 'n '> 'r 'n "}"))
+
+(jdee-gen-define-abbrev-template "sysin"  '("System.in.readline();"))
+(jdee-gen-define-abbrev-template "sysout" '("System.out.println(\"" (p "x:") "\");"))
+(jdee-gen-define-abbrev-template "syserr" '("System.err.println(\"" (p "x:") "\");"))
+(jdee-gen-define-abbrev-template "trace"  '("logger.trace(\"" (p "Message:") "\");"))
+(jdee-gen-define-abbrev-template "debug"  '("logger.debug(\"" (p "Message:") "\");"))
+(jdee-gen-define-abbrev-template "info"   '("logger.info(\"" (p "Message:") "\");"))
+(jdee-gen-define-abbrev-template "warn"   '("logger.warn(\"" (p "Message:") "\");"))
+(jdee-gen-define-abbrev-template "error"  '("logger.error(\"" (p "Message:") "\");"))
+(jdee-gen-define-abbrev-template "fatal"  '("logger.fatal(\"" (p "Message:") "\");"))
 
 (tempo-define-template "debug"
                        '("if (debug) {" n> "System.out.println(\"" (p "Enter debug message:") "\");" n> "}")
